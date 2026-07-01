daaed58c1d225114f3f416c293b16184dcb0a30a
Kaushik <kaushik.avks@gmail.com>
2026-07-01 21:58:38 +0530

    config.py

---
+++ Week 2/HandsOn4/config.py
@@ -0,0 +1,9 @@
+class Config:
+    SECRET_KEY = "coursemanager123"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///coursemanager.db"
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    DEBUG = True
+  
