b02f1537792284e92fa0880296ece7a57cca3ddf
Kaushik <kaushik.avks@gmail.com>
2026-07-01 21:45:10 +0530

    urls.py

---
+++ Week 2/HandsOn4/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .views import (
+    CourseListCreateAPIView,
+    CourseRetrieveUpdateDestroyAPIView
+)
+
+urlpatterns = [
+
+    path(
+        "courses/",
+        CourseListCreateAPIView.as_view(),
+        name="course-list"
+    ),
+
+    path(
+        "courses/<int:pk>/",
+        CourseRetrieveUpdateDestroyAPIView.as_view(),
+        name="course-detail"
+    ),
+
+]
