f1f119b05dd0add5add06cdcafbaeffc18a36caa
Kaushik <kaushik.avks@gmail.com>
2026-07-15 13:21:10 +0530

    data.js

---
+++ Week 3/handson_04/data.js
@@ -0,0 +1,42 @@
+// ==========================================
+// LOCAL COURSE DATA
+// ==========================================
+
+export const courses = [
+
+    {
+        id: 1,
+        name: "Data Structures",
+        code: "CS101",
+        credits: 4
+    },
+
+    {
+        id: 2,
+        name: "Python Programming",
+        code: "CS102",
+        credits: 4
+    },
+
+    {
+        id: 3,
+        name: "Database Management",
+        code: "CS103",
+        credits: 3
+    },
+
+    {
+        id: 4,
+        name: "Web Development",
+        code: "CS104",
+        credits: 4
+    },
+
+    {
+        id: 5,
+        name: "Computer Networks",
+        code: "CS105",
+        credits: 3
+    }
+
+];
