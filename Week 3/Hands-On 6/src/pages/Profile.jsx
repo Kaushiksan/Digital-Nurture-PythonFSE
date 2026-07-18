ff06ce2af89377018dcb88fda48e5598268fa724
Kaushik <kaushik.avks@gmail.com>
2026-07-18 16:00:54 +0530

    Profile.jsx

---
+++ Week 3/Hands-On 6/src/pages/Profile.jsx
@@ -0,0 +1,21 @@
+function Profile() {
+
+    return (
+
+        <section className="page">
+
+            <h2>Profile</h2>
+
+            <p>
+
+                Student Profile Page.
+
+            </p>
+
+        </section>
+
+    );
+
+}
+
+export default Profile;
