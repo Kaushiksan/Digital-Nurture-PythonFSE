0d688525cb912baa87f1891d8051dfaebd487ab6
Kaushik <kaushik.avks@gmail.com>
2026-07-18 16:01:25 +0530

    NotFound.jsx

---
+++ Week 3/Hands-On 6/src/pages/NotFound.jsx
@@ -0,0 +1,21 @@
+function NotFound() {
+
+    return (
+
+        <section className="page">
+
+            <h2>404</h2>
+
+            <p>
+
+                Page Not Found.
+
+            </p>
+
+        </section>
+
+    );
+
+}
+
+export default NotFound;
