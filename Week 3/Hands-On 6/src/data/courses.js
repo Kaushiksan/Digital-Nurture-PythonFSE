ccc36025bbad04a8b177397c05c29c4cb5c135d4
Kaushik <kaushik.avks@gmail.com>
2026-07-18 16:02:31 +0530

    courses.js

---
+++ Week 3/Hands-On 6/src/data/courses.js
@@ -0,0 +1,45 @@
+const courses = [
+
+    {
+        id: 1,
+        name: "Python Programming",
+        code: "CS101",
+        credits: 4,
+        instructor: "Dr. Kumar"
+    },
+
+    {
+        id: 2,
+        name: "Java Programming",
+        code: "CS102",
+        credits: 4,
+        instructor: "Dr. Priya"
+    },
+
+    {
+        id: 3,
+        name: "Web Development",
+        code: "CS103",
+        credits: 3,
+        instructor: "Dr. Arun"
+    },
+
+    {
+        id: 4,
+        name: "Database Systems",
+        code: "CS104",
+        credits: 3,
+        instructor: "Dr. Meena"
+    },
+
+    {
+        id: 5,
+        name: "React Development",
+        code: "CS105",
+        credits: 4,
+        instructor: "Dr. Ravi"
+    }
+
+];
+
+export default courses;
