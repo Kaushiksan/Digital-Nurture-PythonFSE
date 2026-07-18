a34aa914e5e19f3792e461cea896bc5cd2763916
Kaushik <kaushik.avks@gmail.com>
2026-07-18 16:00:26 +0530

    Courses.jsx

---
+++ Week 3/Hands-On 6/src/pages/Courses.jsx
@@ -0,0 +1,21 @@
+function Courses() {
+
+    return (
+
+        <section className="page">
+
+            <h2>Courses</h2>
+
+            <p>
+
+                Available Courses will be displayed here.
+
+            </p>
+
+        </section>
+
+    );
+
+}
+
+export default Courses;
