bfba9ac2bb0cc0176a2e76b230da98e3830279a2
Kaushik <kaushik.avks@gmail.com>
2026-07-18 16:00:05 +0530

    Home.jsx

---
+++ Week 3/Hands-On 6/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+function Home() {
+
+    return (
+
+        <section className="page">
+
+            <h2>Home</h2>
+
+            <p>
+
+                Welcome to the Student Portal.
+
+            </p>
+
+        </section>
+
+    );
+
+}
+
+export default Home;
