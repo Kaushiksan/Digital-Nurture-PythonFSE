b06b8c00eabed855fde1a99ea0c16a0027ba382c
Kaushik <kaushik.avks@gmail.com>
2026-07-18 15:58:46 +0530

    App.jsx

---
+++ Week 3/Hands-On 6/src/App.jsx
@@ -0,0 +1,57 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Courses from "./pages/Courses";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+import "./App.css";
+
+function App() {
+
+    return (
+
+        <>
+
+            <Header />
+
+            <main>
+
+                <Routes>
+
+                    <Route
+                        path="/"
+                        element={<Home />}
+                    />
+
+                    <Route
+                        path="/courses"
+                        element={<Courses />}
+                    />
+
+                    <Route
+                        path="/profile"
+                        element={<Profile />}
+                    />
+
+                    <Route
+                        path="*"
+                        element={<NotFound />}
+                    />
+
+                </Routes>
+
+            </main>
+
+            <Footer />
+
+        </>
+
+    );
+
+}
+
+export default App;
