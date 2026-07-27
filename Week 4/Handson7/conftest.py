7cb9fea65b534de270e2f105554b17a30d84e229
Kaushik <kaushik.avks@gmail.com>
2026-07-27 13:02:00 +0530

    conftest.py

--- Week 4/Handson7/conftest.py
+++ Week 4/Handson7/conftest.py
@@ -1,13 +1,25 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def base_url():
+    return "https://www.lambdatest.com/selenium-playground/"
+
+
+@pytest.fixture(scope="function")
 def driver():
 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager().install()
+        )
+    )
 
     driver.maximize_window()
+    driver.implicitly_wait(10)
 
     yield driver
 
